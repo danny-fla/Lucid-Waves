bdc628e2aa2c4ac962ddead7ff710cc8345296c2
danny-fla <dannyfla@live.ie>
2023-09-21 09:27:48 +0000

    deployment commit

--- LUCID_WAVES/settings.py
+++ LUCID_WAVES/settings.py
@@ -14,10 +14,11 @@ from pathlib import Path
 import os
 import dj_database_url
 if os.path.isfile('env.py'):
-     import env
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -29,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-dannyfla-lucidwaves-kpxwlunb6wf.ws-eu104.gitpod.io']
+ALLOWED_HOSTS = ['8000-dannyfla-lucidwaves-kpxwlunb6wf.ws-eu104.gitpod.io', 'LUCID_WAVES.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -41,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'django.contrib.staticfiles',
+    'cloudinary',
     'blog',
 ]
 
@@ -59,7 +63,7 @@ ROOT_URLCONF = 'LUCID_WAVES.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,6 +130,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
