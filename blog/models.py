7b5d9adfdcdc909b1eea55c1c548f79925f76715
danny-fla <dannyfla@live.ie>
2023-10-10 16:33:04 +0000

    install Pillow

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,9 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    header_image = models.ImageField(null=True, blank=True, upload_to="images/")
+    header_image = models.ImageField(
+        null=True, blank=True, upload_to="images/"
+    )
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
