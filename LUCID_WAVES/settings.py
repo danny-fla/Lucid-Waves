769cd9fa79804e77612bc5017a867d942e0f0502
danny-fla <dannyfla@live.ie>
2023-10-12 13:48:26 +0000

    make changes to footer

--- LUCID_WAVES/settings.py
+++ LUCID_WAVES/settings.py
@@ -179,7 +179,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
