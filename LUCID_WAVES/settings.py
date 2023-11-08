3d279ad27f5f64a7f17e7c33a56e25de38dca9b8
danny-fla <dannyfla@live.ie>
2023-11-08 17:08:46 +0000

    add new entry to 'allowed hosts'. remove deprecated database code from settings

--- LUCID_WAVES/settings.py
+++ LUCID_WAVES/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['LUCID_WAVES.herokuapp.com', 'localhost',  '8000-dannyfla-lucidwaves-kpxwlunb6wf.ws-eu105.gitpod.io']
+ALLOWED_HOSTS = ['LUCID_WAVES.herokuapp.com', 'localhost',  '8000-dannyfla-lucidwaves-kpxwlunb6wf.ws-eu106.gitpod.io']
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
@@ -125,12 +125,6 @@ ACCOUNT_DEFAULT_HTTP_PROTOCOL='https'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
      'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
