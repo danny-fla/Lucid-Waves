d1c8e8f0e5c81d98257d103dd84f9d8f539cf09d
danny-fla <dannyfla@live.ie>
2023-11-23 19:07:29 +0000

    set debug tag to false

--- LUCID_WAVES/settings.py
+++ LUCID_WAVES/settings.py
@@ -32,7 +32,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['LUCID_WAVES.herokuapp.com', 'localhost',  '8000-dannyfla-lucidwaves-kpxwlunb6wf.ws-eu106.gitpod.io']
 
